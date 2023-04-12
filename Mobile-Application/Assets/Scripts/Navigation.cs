dd9fcc9508609b32e57a633d871549744675f2e4
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-04-12 15:52:42 +0530

    navigation bar in mobile application

--- Mobile-Application/Assets/Scripts/Navigation.cs
+++ Mobile-Application/Assets/Scripts/Navigation.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Navigation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Home_Panel;
+    public GameObject Promotion_Panel;
+    public GameObject Search_Panel;
+    public GameObject Account_Panel;
 
-    // Update is called once per frame
-    void Update()
+
+    public GameObject[] panels;
+
+    public void home()
     {
-        
+
+        Home_Panel.SetActive(true);
+        Promotion_Panel.SetActive(false);
+        Search_Panel.SetActive(false);
+        Account_Panel.SetActive(false);
+    }
+    public void promotion()
+    {
+        Home_Panel.SetActive(false);
+        Promotion_Panel.SetActive(true);
+        Search_Panel.SetActive(false);
+        Account_Panel.SetActive(false);
+    }
+    public void search()
+    {
+        Home_Panel.SetActive(false);
+        Promotion_Panel.SetActive(false);
+        Search_Panel.SetActive(true);
+        Account_Panel.SetActive(false);
+    }
+    public void account()
+    {
+        Home_Panel.SetActive(false);
+        Promotion_Panel.SetActive(false);
+        Search_Panel.SetActive(false);
+        Account_Panel.SetActive(true);
     }
 }
