0e9bd1a69fb8121594fdea179226f7d1cc2ade60
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-04-12 20:27:26 +0530

    Navigation bar added and complete

--- Mobile-Application/Assets/Scripts/Navigation.cs
+++ Mobile-Application/Assets/Scripts/Navigation.cs
@@ -4,41 +4,51 @@ using UnityEngine;
 
 public class Navigation : MonoBehaviour
 {
-    public GameObject Home_Panel;
-    public GameObject Promotion_Panel;
-    public GameObject Search_Panel;
-    public GameObject Account_Panel;
+    [SerializeField] GameObject[] panels;
+
+    public void NavigationBarClick(GameObject activePanel)
+    {
+        for(int i=0; i<panels.Length; i++)
+        {
+            panels[i].SetActive(false);
+        }
+        activePanel.SetActive(true);
+    }
+    // public GameObject Home_Panel;
+    // public GameObject Promotion_Panel;
+    // public GameObject Search_Panel;
+    // public GameObject Account_Panel;
 
 
-    public GameObject[] panels;
+    // public GameObject[] panels;
 
-    public void home()
-    {
+    // public void home()
+    // {
 
-        Home_Panel.SetActive(true);
-        Promotion_Panel.SetActive(false);
-        Search_Panel.SetActive(false);
-        Account_Panel.SetActive(false);
-    }
-    public void promotion()
-    {
-        Home_Panel.SetActive(false);
-        Promotion_Panel.SetActive(true);
-        Search_Panel.SetActive(false);
-        Account_Panel.SetActive(false);
-    }
-    public void search()
-    {
-        Home_Panel.SetActive(false);
-        Promotion_Panel.SetActive(false);
-        Search_Panel.SetActive(true);
-        Account_Panel.SetActive(false);
-    }
-    public void account()
-    {
-        Home_Panel.SetActive(false);
-        Promotion_Panel.SetActive(false);
-        Search_Panel.SetActive(false);
-        Account_Panel.SetActive(true);
-    }
+    //     Home_Panel.SetActive(true);
+    //     Promotion_Panel.SetActive(false);
+    //     Search_Panel.SetActive(false);
+    //     Account_Panel.SetActive(false);
+    // }
+    // public void promotion()
+    // {
+    //     Home_Panel.SetActive(false);
+    //     Promotion_Panel.SetActive(true);
+    //     Search_Panel.SetActive(false);
+    //     Account_Panel.SetActive(false);
+    // }
+    // public void search()
+    // {
+    //     Home_Panel.SetActive(false);
+    //     Promotion_Panel.SetActive(false);
+    //     Search_Panel.SetActive(true);
+    //     Account_Panel.SetActive(false);
+    // }
+    // public void account()
+    // {
+    //     Home_Panel.SetActive(false);
+    //     Promotion_Panel.SetActive(false);
+    //     Search_Panel.SetActive(false);
+    //     Account_Panel.SetActive(true);
+    // }
 }
