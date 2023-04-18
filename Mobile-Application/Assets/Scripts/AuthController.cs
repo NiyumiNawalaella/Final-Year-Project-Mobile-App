353093202464a6277af1af4c33f76440da396951
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-04-18 14:01:44 +0530

    changes made

---
+++ Mobile-Application/Assets/Scripts/AuthController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Auth;
+
+public class AuthController : MonoBehaviour
+{
+    public Text emailInput, passwordInput;
+}
