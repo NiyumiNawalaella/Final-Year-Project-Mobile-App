331424790964e9b8cb8ac7a866500cd600583d9c
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-04-27 14:32:48 +0530

    navigate to login and sign up page and connection with the database

---
+++ Mobile-Application/Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager Instance;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+    public enum Scene
+    {
+       // AccountCustomerScene,
+        //AccountShopOwnerScene,
+        //AddPromotionScene,
+        //ChangePasswordAccountScene,
+        //EditPageAccountScene,
+        //EditPageShopScene,
+        FirstScene,
+        LoginScene,
+        RegisterScene,
+        RegisterShopScene,
+        HomeScene,
+        WelcomeScene
+        //PermissionScene,
+        //PromotionScene,
+        
+        //SearchingScene,
+        //SearchScene,
+       
+    }
+    public void LoadScene(Scene scene )
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+    
+}
